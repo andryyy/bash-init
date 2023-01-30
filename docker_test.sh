9b2f26607d497c98bad4b221e14e6f82e095202e
andryyy <andre.peters@debinux.de>
2023-01-30 15:11:57 +0100

    Mon 30 Jan 2023 03:11:57 PM CET

--- docker_test.sh
+++ docker_test.sh
@@ -1,8 +1,6 @@
 docker run --rm --name bash-init \
   -v $(pwd):/bash-init \
-  -v $(pwd)/main.py:/granian-app/main.py \
   --stop-timeout=600 \
   --pids-limit=200 \
   -p 8080:8080 \
-  -p 8081:8081 \
   bash /bash-init/run.sh ${@}
